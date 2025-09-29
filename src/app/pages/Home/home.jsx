65e6482962e49b9517ec3b594e52d75b798cc95f
Пользователь <sobirjonsharipov590@gmail.com>
2025-09-29 09:58:13 +0500

    Fon

--- src/app/pages/Home/home.jsx
+++ src/app/pages/Home/home.jsx
@@ -13,27 +13,27 @@ const Home = () => {
       <div className='flex justify-between items-center'>
         <div>
           <p style={{ fontFamily: "Gilroy-Light",fontSize:"16px",color:"#8696BB" }}>Добро пожаловать,</p>
-          <p style={{ fontFamily: "Gilroy-ExtraBold",fontSmooth:"20px",color:"#0D1B34" }}>Фирдавс Намозов</p>
+          <p style={{ fontFamily: "Gilroy-ExtraBold",fontSize:"20px",color:"#0D1B34" }}>Фирдавс Намозов</p>
         </div>
         <img src={img} alt="" />
       </div>
 
       <div className='bg-[#005456] text-white p-[20px] rounded-[10px] mt-[30px]'>
-        <p style={{ fontFamily: "Gilroy-Ligght",fontSize:"16px" }}>Ваша касса на данный момент</p>
-        <p style={{ fontFamily: "Gilroy-Ligght",fontSize:"14px",color:"#CBE1FF" }}>+ 3500 сомони</p>
+        <p style={{ fontFamily: "Gilroy-Light",fontSize:"16px" }}>Ваша касса на данный момент</p>
+        <p style={{ fontFamily: "Gilroy-Light",fontSize:"14px",color:"#CBE1FF" }}>+ 3500 сомони</p>
         <hr className='my-[20px]' />
         <div className='flex justify-between '>
           <div className='flex gap-[10px] items-center'>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
             </svg>
-            <p style={{ fontFamily: "Gilroy-Ligght",fontSize:"12px" }}>Sunday, 12 June</p>
+            <p style={{ fontFamily: "Gilroy-Light",fontSize:"12px" }}>Sunday, 12 June</p>
           </div>
           <div className='flex gap-[10px] items-center'>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
             </svg>
-            <p style={{ fontFamily: "Gilroy-Ligght",fontSize:"12px" }}>11:00 - Dushanbe</p>
+            <p style={{ fontFamily: "Gilroy-Light",fontSize:"12px" }}>11:00 - Dushanbe</p>
           </div>
         </div>
       </div>
@@ -61,7 +61,7 @@ const Home = () => {
           <div className='bg-[#005456] flex justify-center p-[20px] rounded-[10px]'>
             <img draggable={false} src={img1} alt="" />
           </div>
-          <p style={{ fontFamily: "Gilroy-Ligght" }} className='mt-[10px]'>Курьеры</p>
+          <p style={{ fontFamily: "Gilroy-Light" }} className='mt-[10px]'>Курьеры</p>
         </div>
       </div>
 
